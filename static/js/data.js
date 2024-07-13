308abc1c183ece34f4e4335ef0e130da0662bab6
jh954 <84773633+jh954@users.noreply.github.com>
2024-07-13 17:40:12 +0800

    Add files via upload

---
+++ static/js/data.js
@@ -0,0 +1,15 @@
+const routes = [
+  {name: '虾米解析', url: 'https://jx.xmflv.com/?url='},
+  {name: 'BL解析', url: 'https://svip.bljiex.cc/?v='}, 
+  {name: 'JSON解析', url: 'https://jx.jsonplayer.com/player/?url='},
+  {name: '影猫の仓库 ', url: 'https://ymck.pro/search.html?wd='}, 
+  
+]
+
+const friends = [
+  {name: 'Here200\'s Bookmark', url: 'https://here200.github.io/Bookmark/'},
+]
+const other = [
+  {name: '夜幕', url: 'https://yemu.xyz/shouye/'},
+  {name: 'LQQ', url: 'https://v.ctrlqq.com/'},
+]
