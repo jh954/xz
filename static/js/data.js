0a219e18e7dc7ad45a2dfa89c8290ae5e23ae7a6
jh954 <84773633+jh954@users.noreply.github.com>
2024-07-13 19:00:52 +0800

    Update data.js

--- static/js/data.js
+++ static/js/data.js
@@ -1,8 +1,13 @@
 const routes = [
   {name: '虾米解析', url: 'https://jx.xmflv.com/?url='},
+  {name:"纯净1",url:"https://im1907.top/?jx="},
   {name: 'BL解析', url: 'https://svip.bljiex.cc/?v='}, 
-  {name: 'JSON解析', url: 'https://jx.jsonplayer.com/player/?url='},
-  {name: '影猫の仓库 ', url: 'https://ymck.pro/search.html?wd='}, 
+  {name:"七哥",url:"https://jx.nnxv.cn/tv.php?url="},
+  {name:"冰豆",url:"https://bd.jx.cn/?url="},
+  {name:"M3U8",url:"https://jx.m3u8.tv/jiexi/?url="},
+  {name:"JY",url:"https://jx.playerjy.com/?url="},
+  {name:"8090",url:"https://www.8090g.cn/?url="},
+  {name:"PM",url:"https://www.playm3u8.cn/jiexi.php?url="},
   
 ]
 
@@ -12,4 +17,6 @@ const friends = [
 const other = [
   {name: '夜幕', url: 'https://yemu.xyz/shouye/'},
   {name: 'LQQ', url: 'https://v.ctrlqq.com/'},
+  {name: '影猫の仓库 ', url: 'https://ymck.pro/search.html?wd='}, 
+  {name:"云析",url:"https://jx.yparse.com/index.php?url="},
 ]
